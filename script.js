03bd511336715a79e7ed877f2187d0247bb3931f
mlynekit <45061656+mlynekit@users.noreply.github.com>
2019-05-15 23:02:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    $("#a").click(function(){
+        $("#img1").attr("src","zaun.png");
+        $(".side h1").text("Zaun")
+        $(".side p").hide();
+        $("#img2").attr("src","zaun2.jpg");
+    });
+    $("#b").click(function(){
+        $("#img1").attr("src","piltover.png");
+        $(".side h1").text("Piltover")
+        $(".side p").hide();
+        $("#img2").attr("src","piltover2.jpg");
+    });
+    $("#c").click(function(){
+        $("#img1").attr("src","bilgewater.png");
+        $(".side h1").text("Bilgewater")
+        $(".side p").hide();
+        $("#img2").attr("src","bilgewater2.jpg");
+    });
+    $("#d").click(function(){
+        $("#img1").attr("src","shadow.png");
+        $(".side h1").text("Shadow Isles")
+        $(".side p").hide();
+        $("#img2").attr("src","shadow2.jpg");
+    });
+    $("#e").click(function(){
+        $("#img1").attr("src","ionia.png");
+        $(".side h1").text("Ionia")
+        $(".side p").hide();
+        $("#img2").attr("src","ionia2.jpg");
+    });
+    $("#f").click(function(){
+        $("#img1").attr("src","freljord.png");
+        $(".side h1").text("Freljord")
+        $(".side p").hide();
+        $("#img2").attr("src","freljord2.jpg");
+    });
+    $("#g").click(function(){
+        $("#img1").attr("src","noxus.png");
+        $(".side h1").text("Noxus")
+        $(".side p").hide();
+        $("#img2").attr("src","noxus2.jpg");
+    });
+    $("#h").click(function(){
+        $("#img1").attr("src","demacia.png");
+        $(".side h1").text("Demacia")
+        $(".side p").hide();
+        $("#img2").attr("src","demacia2.jpg");
+    });
+    $("#i").click(function(){
+        $("#img1").attr("src","shurima.png");
+        $(".side h1").text("Shurima")
+        $(".side p").hide();
+        $("#img2").attr("src","shurima2.jpg");
+    });
+    $("#j").click(function(){
+        $("#img1").attr("src","targon.png");
+        $(".side h1").text("Targon")
+        $(".side p").hide();
+        $("#img2").attr("src","targon2.jpg");
+    });
+  });
